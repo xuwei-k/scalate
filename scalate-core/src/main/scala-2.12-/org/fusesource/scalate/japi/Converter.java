3b8cc01aa510807e7f11d535e648f65721cf2033
xuwei-k <6b656e6a69@gmail.com>
2018-05-29 14:11:00 +0900

    wip

---
+++ scalate-core/src/main/scala-2.12-/org/fusesource/scalate/japi/Converter.java
@@ -0,0 +1,15 @@
+package org.fusesource.scalate.japi;
+
+import scala.collection.JavaConversions;
+import scala.collection.mutable.Buffer;
+import java.util.List;
+
+final class Converter {
+  static <A> Buffer<A> asScalaBuffer(List<A> list) {
+    return JavaConversions.asScalaBuffer(list);
+  }
+
+  static <A, B> scala.collection.mutable.Map<A, B> mapAsScalaMap(java.util.Map<A, B> map) {
+    return JavaConversions.mapAsScalaMap(map);
+  }
+}
